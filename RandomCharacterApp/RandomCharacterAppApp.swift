5c6dba4f53106d631824b9dd2c299ef2b601c865
arifinfrds <arifinfrds.engineer@gmail.com>
2023-04-07 14:06:30 +0700

    Initial Commit

---
+++ RandomCharacterApp/RandomCharacterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomCharacterAppApp.swift
+//  RandomCharacterApp
+//
+//  Created by arifinfrds.engineer on 07/04/23.
+//
+
+import SwiftUI
+
+@main
+struct RandomCharacterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
