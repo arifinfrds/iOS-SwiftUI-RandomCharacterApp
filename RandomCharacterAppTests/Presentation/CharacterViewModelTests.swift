a7bbb0e64817e665d1999fbcc21f448ead7933a7
Arifin Firdaus <arifinfrds.engineer@gmail.com>
2023-06-10 14:41:52 +0700

    refactor SUT creation

--- RandomCharacterAppTests/Presentation/CharacterViewModelTests.swift
+++ RandomCharacterAppTests/Presentation/CharacterViewModelTests.swift
@@ -41,15 +41,13 @@ final class CharacterViewModelTests: XCTestCase {
     private var cancellables = Set<AnyCancellable>()
 
     func test_init_doesNotPerformRequest() {
-        let service = SpyCharacterService()
-        _ = CharacterViewModel(characterService: service)
+        let (_, service) = makeSUT()
         
         XCTAssertEqual(service.loadUserCallCount, 0)
     }
     
     func test_onLoad_performRequest() async {
-        let service = SpyCharacterService()
-        let sut = CharacterViewModel(characterService: service)
+        let (sut, service) = makeSUT()
         
         await sut.onLoad(id: 1)
         
@@ -57,9 +55,8 @@ final class CharacterViewModelTests: XCTestCase {
     }
     
     func test_onLoad_deliversCharacter() async {
-        let expectedCharacter = Character(id: 0, name: "", status: "", species: "", gender: "", image: URL(string: "www.google.com")!)
-        let service = StubCharacterService(result: .success(expectedCharacter))
-        let sut = CharacterViewModel(characterService: service)
+        let expectedCharacter = anyCharacter()
+        let sut = makeSUT(result: .success(expectedCharacter))
         var receivedStates = [CharacterViewModel.State]()
         sut.$state.dropFirst().sink { state in
             receivedStates.append(state)
@@ -75,8 +72,7 @@ final class CharacterViewModelTests: XCTestCase {
     func test_onLoad_deliversError() async {
         let errors = RemoteCharacterService.Error.allCases
         for (index, error) in errors.enumerated() {
-            let service = StubCharacterService(result: .failure(error))
-            let sut = CharacterViewModel(characterService: service)
+            let sut = makeSUT(result: .failure(error))
             var receivedStates = [CharacterViewModel.State]()
             sut.$state.dropFirst().sink { state in
                 receivedStates.append(state)
@@ -87,6 +83,23 @@ final class CharacterViewModelTests: XCTestCase {
 
             XCTAssertEqual(receivedStates, [.loading, .error], "Expect error, got fail instead at index: \(index), error: \(error)")
         }
+    }
+    
+    // MARK: - Helpers
+    
+    private func makeSUT() -> (sut: CharacterViewModel, SpyCharacterService) {
+        let service = SpyCharacterService()
+        let sut = CharacterViewModel(characterService: service)
+        return (sut, service)
+    }
+    
+    private func makeSUT(result: Result<Character, Error>) -> CharacterViewModel {
+        let service = StubCharacterService(result: result)
+        return CharacterViewModel(characterService: service)
+    }
+    
+    private func anyCharacter() -> Character {
+        Character(id: 0, name: "", status: "", species: "", gender: "", image: URL(string: "www.google.com")!)
     }
 }
 
