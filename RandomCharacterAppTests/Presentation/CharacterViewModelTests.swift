9029eb5449ab01b3781eac23316d0acaa013278b
Arifin Firdaus <arifinfrds.engineer@gmail.com>
2023-06-10 10:48:43 +0700

    Delivers error and character

--- RandomCharacterAppTests/Presentation/CharacterViewModelTests.swift
+++ RandomCharacterAppTests/Presentation/CharacterViewModelTests.swift
@@ -11,12 +11,21 @@ import XCTest
 final class CharacterViewModel {
     private let characterService: CharacterService
     
+    var shouldShowError = false
+    var errorMessage: String? = nil
+    var character: Character? = nil
+    
     init(characterService: CharacterService) {
         self.characterService = characterService
     }
     
     func onLoad(id: Int) async {
-        _ = try? await characterService.load(id: id)
+        do {
+            character = try await characterService.load(id: id)
+        } catch {
+            shouldShowError = true
+            errorMessage = "Something went wrong, please try again"
+        }
     }
 }
 
@@ -37,6 +46,26 @@ final class CharacterViewModelTests: XCTestCase {
         
         XCTAssertEqual(service.loadUserCallCount, 1)
     }
+    
+    func test_onLoad_deliversCharacter() async {
+        let expectedCharacter = Character(id: 0, name: "", status: "", species: "", gender: "", image: URL(string: "www.google.com")!)
+        let service = StubCharacterService(result: .success(expectedCharacter))
+        let sut = CharacterViewModel(characterService: service)
+        
+        await sut.onLoad(id: 1)
+        
+        XCTAssertEqual(sut.character, expectedCharacter)
+    }
+    
+    func test_onLoad_deliversError() async {
+        let service = StubCharacterService(result: .failure(RemoteCharacterService.Error.invalidJSONError))
+        let sut = CharacterViewModel(characterService: service)
+        
+        await sut.onLoad(id: 1)
+        
+        XCTAssertEqual(sut.shouldShowError, true)
+        XCTAssertEqual(sut.errorMessage, "Something went wrong, please try again")
+    }
 }
 
 final class SpyCharacterService: CharacterService {
@@ -46,5 +75,22 @@ final class SpyCharacterService: CharacterService {
     func load(id: Int) async throws -> Character {
         loadUserCallCount += 1
         return Character(id: 0, name: "", status: "", species: "", gender: "", image: URL(string: "www.google.com")!)
+    }
+}
+
+final class StubCharacterService: CharacterService {
+    private let result: Result<Character, Error>
+    
+    init(result: Result<Character, Error>) {
+        self.result = result
+    }
+    
+    func load(id: Int) async throws -> Character {
+        switch result {
+        case .success(let character):
+            return character
+        case .failure(let error):
+            throw error
+        }
     }
 }
