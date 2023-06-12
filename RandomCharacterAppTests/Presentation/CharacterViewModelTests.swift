a05788d4eb733aa1125e7244e2c84c6dc09c1718
Arifin Firdaus <arifinfrds.engineer@gmail.com>
2023-06-13 06:31:47 +0700

    `CharacterViewModel.init` does not load user

---
+++ RandomCharacterAppTests/Presentation/CharacterViewModelTests.swift
@@ -0,0 +1,37 @@
+//
+//  CharacterViewModelTests.swift
+//  RandomCharacterAppTests
+//
+//  Created by arifin on 13/06/23.
+//
+
+import XCTest
+@testable import RandomCharacterApp
+
+final class CharacterViewModel {
+    private let characterService: CharacterService
+    
+    init(characterService: CharacterService) {
+        self.characterService = characterService
+    }
+}
+
+final class CharacterViewModelTests: XCTestCase {
+
+    func test_init_doesNotLoadUser() {
+        let service = ServiceSpy()
+        let sut = CharacterViewModel(characterService: service)
+        
+        XCTAssertEqual(service.loadUserCallCount, 0)
+    }
+    
+    private final class ServiceSpy: CharacterService {
+        private(set) var loadUserCallCount = 0
+        
+        func load(id: Int) async throws -> Character {
+            loadUserCallCount += 1
+            return Character(id: 0, name: "", status: "", species: "", gender: "", image: URL(string: "www.google.com")!)
+        }
+    }
+
+}
